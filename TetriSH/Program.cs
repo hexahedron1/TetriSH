9056557d0b6142a3a1dcbf292cc1917b4b204d6b
hexahedron1 <elongatedsquareorthobicupola@gmail.com>
2025-09-24 21:25:59 +0300

    Settings saving

--- TetriSH/Program.cs
+++ TetriSH/Program.cs
@@ -1,22 +1,36 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Net.Http.Json;
+using Newtonsoft.Json;
 using PopupLib;
 
 string[] splashes = [
     "Fun!",
-    "Welcome to hell",
+    "Welcome to (s)hell",
     "So you have chosen, pain",
     "The least practical shell!",
     "...",
     "Whose idea was this?"
 ];
-
+string splash = splashes[new Random().Next(splashes.Length)];
 string menu = "main"; // this is probably a dumb way of handling menus but i don't give a damn honestly
-string exec = "/bin/bash -c {0}";
+string datadir = $"/home/{Environment.UserName}/.local/share/tetrish";
+if (!Directory.Exists(datadir)) {
+    Directory.CreateDirectory(datadir);
+}
+Data savedata = new(); 
+if (File.Exists(Path.Join(datadir, "data.json"))) {
+    string json = File.ReadAllText(Path.Join(datadir, "data.json"));
+    Data? notjson = JsonConvert.DeserializeObject<Data>(json);
+    if (notjson is not null)
+        savedata = notjson;
+} else {
+    SaveData();
+}
 while (true) {
     switch (menu) {
         case "main": {
-            string select = SelectPopup.Quick(splashes[new Random().Next(splashes.Length)], [ "Run", "Settings", "Stats", "Quit" ], title: "TetriSH");
+            string select = SelectPopup.Quick(splash, [ "Run", "Settings", "Stats", "Quit" ], title: "TetriSH");
             switch (select) {
                 case "Settings": menu = "settings"; break;
                 case "Quit": {
@@ -26,15 +40,31 @@ while (true) {
                     break;
                 }
             }
-
             break;
         }
         case "settings": {
-            string select = SelectPopup.Quick("Settings", ["Shell executable", "Back"], title: "TetriSH");
+            string select = SelectPopup.Quick("Settings", ["Shell", "Back"], title: "TetriSH");
             switch (select) {
+                case "Shell": {
+                    savedata.Exec = TextPromptPopup.Quick($"Current: {savedata.Exec}\nThe shell executable path and arguments for running commands. {{0}} will be replaced with the line that has been cleared", placeholder: "type here", title: "TetriSH | Shell");
+                    SaveData();
+                    break;
+                }
                 case "Back": menu = "main"; break;
             }
             break;
         }
     }
+}
+
+void SaveData() {
+    string json = JsonConvert.SerializeObject(savedata);
+    File.WriteAllText(Path.Join(datadir, "data.json"), json);
+}
+
+class Data {
+    public Data() {
+        Exec = "/bin/bash -c {0}";
+    }
+    public string Exec { get; set; }
 }
